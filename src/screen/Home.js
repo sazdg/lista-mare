bb7fff69ca63fa050bf4ecde4e9aba98c2f9fc08
Sara <saradegra@gmail.com>
2022-07-28 16:17:11 +0200

    Homepage

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -7,10 +7,16 @@ function Home() {
         <div className="homepage">
             <h1>PACKING CHECKLIST</h1>
             <p>Welcome!</p>
+            <span>
+                N accounts<br/>
+                1 list
+            </span>
             <div style={{visibility: 'hidden'}}>
                 <p>footer</p>
                 <p>bug quando metti il filtro e poi clicchi usato</p>
                 <p>feedback quando si aggiunge item o categoria</p>
+                <p>pagina di registrazione nuovo utente</p>
+                <p>create table con nuova lista per l'utente</p>
             </div>
             
         </div>
